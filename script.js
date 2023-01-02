bb5a10819bab5f90fac17c1fa3c8c32e963ddfa3
Kevin Özkaraca <80677798+kevinozkaraca@users.noreply.github.com>
2023-01-03 00:40:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+/*
+------------ LA NOTATION BIG O 
+
+La notation Big O est une notation utilisée en algorithmique pour décrire 
+la complexité d'un algorithme. Elle permet de déterminer le temps d'exécution et 
+l'espace mémoire nécessaire à un algorithme en fonction de la taille des données d'entrée.
+
+*/
